059b812cf5b3ced7f924a23b8d61b4f10ea08eca
Lucie <luciegarnier59@gmail.com>
2023-11-08 18:08:59 +0100

    Tests inutiles

--- src/test/java/org/polytech/covid/Teest.java
+++ src/test/java/org/polytech/covid/Teest.java
@@ -1,14 +1,31 @@
 package org.polytech.covid;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class Teest {
+    public static int a;
+    public static int b;
+    public static int c;
+
+    @BeforeAll
+    public static void setup() {
+        a = 1;
+        b = 2;
+        c = a + b;
+    }
 
     @Test
     public void test1() {
-        fail("Not yet implemented");
+        Assertions.assertThat(c).isEqualTo(4);
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertThat(c).isEqualTo(3);
+
     }
 }
