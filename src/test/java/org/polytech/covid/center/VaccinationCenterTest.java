e59dc4e9451870c2d977da670eef8abecdd1f09b
Lucie <luciegarnier59@gmail.com>
2023-11-13 09:05:31 +0100

    Add tests centers

--- src/test/java/org/polytech/covid/center/VaccinationCenterTest.java
+++ src/test/java/org/polytech/covid/center/VaccinationCenterTest.java
@@ -5,12 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.polytech.covid.domain.VaccinationCenter;
 import org.polytech.covid.security.services.VaccinationCenterService;
 
 @SpringBootTest
@@ -29,14 +33,27 @@ public class VaccinationCenterTest {
     }
 
     @Test
-    public void testPostCenter() throws Exception {
-        mockMvc.perform(post("/public/centers")).andExpect(status().isOk());
+    public void testGetCenter() throws Exception {
+        mockMvc.perform(get("/public/center/1")).andExpect(status().isOk());
     }
 
     @Test
-    public void testGetCenter() throws Exception {
-        mockMvc.perform(get("/public/centers/1")).andExpect(status().isOk());
+    public void testPostCenter() throws Exception {
+        mockMvc.perform(post("/public/center/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"name\":\"centerTest\",\"address\":\"addressTest\",\"city\":\"cityTest\",\"postalCode\":\"postalCodeTest\"}"))
+            .andExpect(status().isCreated());
     }
+
+    @Test
+    public void testPostCenterFailed() throws Exception {
+        mockMvc.perform(post("/public/center/")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"name\":\"centerTest\",\"address\":\"addressTest\",\"city\":\"cityTest\",\"postalCode\":\"postalCodeTest\"}"))
+            .andExpect(status().isCreated());
+    }
+
+
 
 
 }
