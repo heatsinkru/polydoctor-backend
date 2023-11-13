32a5cae59eb3ec2c080ef666a51fb964b72ad27b
Lucie <luciegarnier59@gmail.com>
2023-11-13 14:14:28 +0100

    Add tests

--- src/test/java/org/polytech/covid/center/VaccinationCenterTest.java
+++ src/test/java/org/polytech/covid/center/VaccinationCenterTest.java
@@ -1,5 +1,6 @@
 package org.polytech.covid.center;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,9 +11,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.polytech.covid.domain.VaccinationCenter;
 import org.polytech.covid.security.services.VaccinationCenterService;
@@ -26,33 +31,99 @@ public class VaccinationCenterTest {
 
     @MockBean
     private VaccinationCenterService vaccinationCenterService;
+    private static VaccinationCenter center1;
+    private static VaccinationCenter center2;
+    private static List<VaccinationCenter> centers;
+
+    @BeforeAll
+    public static void setup() {
+        center1 = new VaccinationCenter("centerTest1", "addressTest1", "cityTest1", "postalCodeTest1");
+        center2 = new VaccinationCenter("centerTest2", "addressTest2", "cityTest2", "postalCodeTest2");
+        centers =  new java.util.ArrayList<VaccinationCenter>();
+        centers.add(center1);
+        centers.add(center2);
+    }
 
     @Test
     public void testGetCenters() throws Exception {
-        mockMvc.perform(get("/public/centers")).andExpect(status().isOk());
+        doReturn(centers).when(vaccinationCenterService).findAll();
+        mockMvc.perform(get("/public/centers"))
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.content().json("{'data':[{'id':1,'name':'centerTest1','address':'addressTest1','city':'cityTest1','postalCode':'postalCodeTest1','rdvs':[]}," +
+                    "{'id':2,'name':'centerTest2','address':'addressTest2','city':'cityTest2','postalCode':'postalCodeTest2','rdvs':[]}]}"));
     }
 
     @Test
     public void testGetCenter() throws Exception {
-        mockMvc.perform(get("/public/center/1")).andExpect(status().isOk());
+        doReturn(center1).when(vaccinationCenterService).findById(1);
+        mockMvc.perform(get("/public/center/1"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+            // .andExpect(MockMvcResultMatchers.content().json("{'id':1,'name':'centerTest1','address':'addressTest1','city':'cityTest1','postalCode':'postalCodeTest1','rdvs':[]}"));
     }
+
+    @Test
+    public void testGetCenterUnknownId() throws Exception {
+        mockMvc.perform(get("/public/center/1000000")).andExpect(status().isNotFound());
+    }
+
 
     @Test
     public void testPostCenter() throws Exception {
+        // créer @RequestBody VaccinationCenter
         mockMvc.perform(post("/public/center/")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"name\":\"centerTest\",\"address\":\"addressTest\",\"city\":\"cityTest\",\"postalCode\":\"postalCodeTest\"}"))
+            .content("{\"name\":\"centerTest1\",\"address\":\"addressTest1\",\"city\":\"cityTest1\",\"postalCode\":\"postalCodeTest1\"}"))
             .andExpect(status().isCreated());
     }
 
     @Test
-    public void testPostCenterFailed() throws Exception {
+    public void testPostCenterBadRequest() throws Exception {
         mockMvc.perform(post("/public/center/")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"name\":\"centerTest\",\"address\":\"addressTest\",\"city\":\"cityTest\",\"postalCode\":\"postalCodeTest\"}"))
-            .andExpect(status().isCreated());
+            .content("{\"name\":\"centerTest2\",\"address\":\"addressTest2\",\"city\":\"cityTest2\",\"postalCode\":\"postalCodeTest2\",\"autre\":\"postFailed}"))
+            .andExpect(status().isBadRequest());
     }
 
+    @Test
+    public void testPutCenter() throws Exception {
+        doReturn(center1).when(vaccinationCenterService).findById(1);
+        mockMvc.perform(MockMvcRequestBuilders.put("/public/center/1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"id\": 1,\"name\":\"centerTestNew\",\"address\":\"addressTestNew\",\"city\":\"cityTestNew\",\"postalCode\":\"postalCodeTestNew\"}"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testPutCenterBadRequest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/public/center/1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"name\":\"centerTestFailed\",\"address\":\"addressTestFailed\",\"city\":\"cityTestFailed\",\"postalCode\":\"postalCodeTestFailed\",\"autre\":\"putFailed}"))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testPutCenterUnknownId() throws Exception {
+        doReturn(centers).when(vaccinationCenterService).findAll();
+        mockMvc.perform(MockMvcRequestBuilders.put("/public/center/1000000")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"name\":\"centerTestUnknown\",\"address\":\"addressTestUnknown\",\"city\":\"cityTestUnknown\",\"postalCode\":\"postalCodeTestUnknown\"}"))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testDeleteCenter() throws Exception {
+        doReturn(center2).when(vaccinationCenterService).findById(2);
+        mockMvc.perform(MockMvcRequestBuilders.delete("/public/center/2"))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteCenterUnknownId() throws Exception {
+        doReturn(centers).when(vaccinationCenterService).findAll();
+        mockMvc.perform(MockMvcRequestBuilders.delete("/public/center/1000000"))
+            .andExpect(status().isNotFound());
+    }
 
 
 
