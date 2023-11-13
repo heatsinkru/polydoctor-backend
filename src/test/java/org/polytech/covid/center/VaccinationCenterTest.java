92245765480cf9dedfcdcbcaee4a78e0d2c9d153
Lucie <luciegarnier59@gmail.com>
2023-11-13 08:27:41 +0100

    Test get/centers fonctionne !

--- src/test/java/org/polytech/covid/center/VaccinationCenterTest.java
+++ src/test/java/org/polytech/covid/center/VaccinationCenterTest.java
@@ -1,23 +1,20 @@
 package org.polytech.covid.center;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-
-import org.polytech.covid.controller.VaccinationCenterController;
-import org.polytech.covid.domain.VaccinationCenter;
-import org.polytech.covid.security.WebSecurityConfig;
 import org.polytech.covid.security.services.VaccinationCenterService;
 
-@WebMvcTest(controllers = VaccinationCenterController.class)
-@ContextConfiguration(classes = { WebSecurityConfig.class, VaccinationCenterController.class })
+@SpringBootTest
+@AutoConfigureMockMvc
 public class VaccinationCenterTest {
 
     @Autowired
@@ -32,12 +29,14 @@ public class VaccinationCenterTest {
     }
 
     @Test
+    public void testPostCenter() throws Exception {
+        mockMvc.perform(post("/public/centers")).andExpect(status().isOk());
+    }
+
+    @Test
     public void testGetCenter() throws Exception {
         mockMvc.perform(get("/public/centers/1")).andExpect(status().isOk());
     }
 
-    @Test
-    public void testPostCenter() throws Exception {
-        mockMvc.perform(post("/public/centers")).andExpect(status().isOk());
-    }
+
 }
